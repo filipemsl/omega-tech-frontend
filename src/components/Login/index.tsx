eaaec76987b0062e45356edfb152e4f29064838a
filipemsl <filipelima0@live.com>
2021-08-19 08:23:17 -0300

    início do controle de senha / email

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,5 +1,6 @@
 import { Container, Title, SignButton, LoginButton, ButtonContainer } from "./styles";
 import Olho from '../../assets/eye.svg'
+import Invalid from '../../assets/invalid.svg'
 import OlhoFechado from '../../assets/closeeye.svg'
 import { useState } from "react";
 import { ToastContainer, toast, Slide } from 'react-toastify';
@@ -8,26 +9,54 @@ import { useHistory } from "react-router";
 import { Sucesso } from "../Sucesso";
 
 
+
 export function Login() {
+
   const history = useHistory();
   const UserNotFound = () => { toast("Usuário não cadastrado", { transition: Slide }); }
   const [passVisible, setPassVisible] = useState(true);
+  const [emailInvalido, setEmailInvalido] = useState(false);
+  const [senhaInvalida, setSenhaInvalida] = useState(false);
+
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleInputEmail = (e: any) => {
+    setEmail(e.target.value)
+    let check = email
+    console.log(check)
+
+  };
+
+  const handleInputSenha = (e: any) => {
+    setSenha(e.target.value)
+    console.log(senha.length + 1)
+  };
+
+
   return (
     <div className="maincontent shadow-lg rounded-3xl">
       <Title>Login</Title>
+
       <Container>
-        <input type="text" placeholder="E-mail" className=" h-full w-full outline-none" />
+        <input type="text" placeholder="E-mail" value={email} onChange={handleInputEmail} className=" h-full w-full outline-none" />
+        <img src={Invalid} alt="" title="O e-mail digitado é inválido" className={emailInvalido ? "" : "hidden"} />
       </Container>
+
       <Container>
-        <input type={passVisible ? 'password' : 'text'} placeholder="Senha" className=" h-full w-full outline-none" />
+        <input type={passVisible ? 'password' : 'text'} value={senha} onChange={handleInputSenha} placeholder="Senha" className=" h-full w-full outline-none" />
         <button className="w-6" onClick={() => setPassVisible(!passVisible)}>
           <img src={passVisible ? Olho : OlhoFechado} alt="" /></button>
+        <img src={Invalid} alt="" title="A senha digitada é inválida" className={senhaInvalida ? "" : "hidden"} />
       </Container>
+
       <ButtonContainer>
         <LoginButton type='submit' onClick={UserNotFound}>Entrar</LoginButton>
         <SignButton onClick={() => (<Sucesso />)}>Cadastrar</SignButton>
         <ToastContainer position="top-center" />
       </ButtonContainer>
+
+
     </div >
   )
 }
