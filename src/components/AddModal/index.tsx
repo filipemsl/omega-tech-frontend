35866fdcb9d3277de16ad500f16f4cfc71967c0d
filipemsl <filipelima0@live.com>
2021-08-21 13:35:38 -0300

    Update index.tsx

--- src/components/AddModal/index.tsx
+++ src/components/AddModal/index.tsx
@@ -5,58 +5,85 @@ import '../../styles/DatePicker.css';
 import '../../styles/Calendar.css';
 import { FormEvent, useState } from "react";
 import { api } from "../../services/api";
+import { ToastContainer, toast, Slide } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 interface AddModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
-const valor: number = 1500;
+// const valor: number = 1500;
 
 export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
-  const [nome, setNome] = useState('')
-  const [carga, setCarga] = useState([])
-  const [consumo, setConsumo] = useState(0)
-  const [tipo, setTipo] = useState('')
-  const [submercado, setSubmercado] = useState('')
-  const [valueInicio, setValueInicio] = useState(new Date());
-  const [valueFim, setValueFim] = useState(new Date());
+  const ProposalSuccess = () => { toast("Proposta cadastrada com sucesso", { transition: Slide }); }
+  const CargaSuccess = () => { toast("Carga adicionada com sucesso!", { transition: Slide }); }
+  const ProposalError = () => { toast("Erro, tente novamente", { transition: Slide }); }
+  let obj = []
+  const [companyname, setCompanyname] = useState('')
+  const [carga, setCarga] = useState(0);
+  const [charges, setCharges] = useState([])
+  const [kwhconsumption, setKwhconsumption] = useState(0)
+  const [supplytype, setSupplytype] = useState('')
+  const [submarket, setSubmarket] = useState('')
+  const [initialdate, setInitialDate] = useState(new Date());
+  const [finaldate, setFinalDate] = useState(new Date());
+
+  function addValor() {
+    obj.push({ carga, companyname })
+    CargaSuccess()
+  }
+
   function handleCreateNewProposal(event: FormEvent) {
     event.preventDefault();
 
+    let token = localStorage.getItem('access_token'!)
+    let tokenData = token?.replace(/['"]+/g, '')
 
     const data = {
-      nome,
-      carga,
-      consumo,
-      tipo,
-      submercado,
-      valueInicio,
-      valueFim,
+      initialdate,
+      finaldate,
+      charges,
+      supplytype,
+      submarket,
     }
-    api.post('/proposals', data)
+
+    api.post('/proposals', data, { headers: { Authorization: `Bearer ${tokenData}` } })
+      .then(ProposalSuccess, onRequestClose)
+      .catch(ProposalError)
   }
+
   return (
     <Container>
       <h1>Nova proposta</h1>
       <form onSubmit={handleCreateNewProposal}>
         <div className="flex flex-col justify-center align-center">
-          <div className="flex items-center justify-center">
-            <InputContainer className="mt-8">
-              <input type="text" value={nome} onChange={event => setNome(event.target.value)} className="w-full" placeholder="Nome da empresa" />
-            </InputContainer>
-          </div>
-          <div className='flex'>
-            <div className="text-center pt-3 m-0">
-              <h6 className="colorSelect">Carga contratada</h6>
-              <SmallContainer>
 
-                <input type="number" value={carga} className="w-full" />
-              </SmallContainer>
+          <div className="flex items-center justify-center">
+
+            <InputContainer className="mt-8">
+              <input type="text" onChange={event => setCompanyname(event.target.value)} className="w-full" placeholder="Nome da empresa" />
+            </InputContainer>
+
+          </div>
+
+          <div className='flex justify-center'>
+
+            <div className=" flex text-center pt-3 m-0">
+              <div className="flex align-center">
+                <SmallContainer>
+                  <input type="number" value={carga} onChange={event => setCarga(parseInt(event.target.value))} className="w-full" />
+                </SmallContainer>
+              </div>
+              <AddButton type="button" onClick={addValor} className='flex self-center'>Adicionar carga</AddButton>
             </div>
+
+
+          </div>
+          <div className='flex justify-center align-center'>
             <div className="text-center pt-3 m-0">
-              <h6 className="colorSelect">Consumo</h6>
+              <h6 className="colorSelect">Consumo total</h6>
               <SmallContainer>
-                <input type="number" value={consumo} onChange={event => setConsumo(Number(event.target.value))} className="w-full align-center" />
+                <input type="number" value={kwhconsumption} onChange={event => setKwhconsumption(parseInt(event.target.value))} className="w-full align-center" />
               </SmallContainer>
             </div>
           </div>
@@ -65,8 +92,8 @@ export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
               <h6 className="colorSelect">Data de início</h6>
               <SmallContainer>
                 <DatePicker
-                  onChange={setValueInicio}
-                  value={valueInicio}
+                  onChange={setInitialDate}
+                  value={initialdate}
                   className="colorSelect"
                 />
               </SmallContainer>
@@ -76,8 +103,8 @@ export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
               <h6 className="colorSelect">Data final</h6>
               <SmallContainer>
                 <DatePicker
-                  onChange={setValueFim}
-                  value={valueFim}
+                  onChange={setFinalDate}
+                  value={finaldate}
                   className="colorSelect"
                 />
               </SmallContainer>
@@ -87,7 +114,7 @@ export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
           <div className='flex items-center justify-center'>
             <InputContainer className=''>
 
-              <select id="submercados" onChange={event => setSubmercado(event.target.value)} name="cars" className="colorSelect w-full">
+              <select id="submercados" onChange={event => setSubmarket(event.target.value)} name="submercado" className="colorSelect w-full">
                 <option value="">Selecione o submercado</option>
                 <option value="NORTE">Norte</option>
                 <option value="NORDESTE">Nordeste</option>
@@ -103,11 +130,11 @@ export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
             <div className='flex justify-center'>
               <RadioContainer>
                 <div className=''>
-                  <input type="radio" id="convencional" onChange={event => setTipo(event.target.value)} name="tipoenergia" value="convencional" className="m-5 " />
+                  <input type="radio" id="convencional" onChange={event => setSupplytype(event.target.value)} name="tipoenergia" value="convencional" className="m-5 " />
                   <label>Convencional</label><br />
                 </div>
                 <div>
-                  <input type="radio" id="renovavel" onChange={event => setTipo(event.target.value)} name="tipoenergia" value="renovavel" className="m-5" />
+                  <input type="radio" id="renovavel" onChange={event => setSupplytype(event.target.value)} name="tipoenergia" value="renovavel" className="m-5" />
                   <label>Renovável</label><br />
                 </div>
               </RadioContainer>
@@ -128,6 +155,7 @@ export function AddModal({ isOpen, onRequestClose }: AddModalProps) {
         </div>
       </form>
       <img src={logoOmega} alt="" width="120" className="mt-10" />
+      <ToastContainer position="top-center" />
     </Container >
   )
 }
